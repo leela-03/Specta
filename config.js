0904db5190181ce9cd93f3bf6192e396704b5569
Leela <leelamohan5264@gmail.com>
2021-07-05 15:56:41 +0530

    added files

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDV4JG7qjUrZuERkxpDt1Kj9V5ZRlUdlGE",
+    authDomain: "specta-37b1d.firebaseapp.com",
+    databaseURL: "https://specta-37b1d-default-rtdb.firebaseio.com",
+    projectId: "specta-37b1d",
+    storageBucket: "specta-37b1d.appspot.com",
+    messagingSenderId: "785161392157",
+    appId: "1:785161392157:web:99ebb63a66e4e215bff6f4"
+  };
